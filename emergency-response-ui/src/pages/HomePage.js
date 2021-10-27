cedd255729673e64905057760a0f73814d00e909
philip-peiffer <philip.peiffer@gmail.com>
2021-10-26 20:30:29 -0600

    deleted duplicate links on homepage

--- emergency-response-ui/src/pages/HomePage.js
+++ emergency-response-ui/src/pages/HomePage.js
@@ -5,11 +5,6 @@ function HomePage() {
     return(
         <div>
             <h1> HOMEPAGE </h1>
-            <Link to="/employees">Employees Page</Link>
-            <Link to="/shifts">Shifts Page</Link>
-            <Link to="/call-logs">Call Logs Page</Link>
-            <Link to="/reports">Reports Page</Link>
-
             <div>
                 <Link to="/employees">Employees Page</Link>
             </div>
