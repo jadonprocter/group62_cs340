4b43d522693d661828b5a6ffcd487982bcdd500a
Jadon Procter <procteja@flip3.engr.oregonstate.edu>
2021-11-18 04:57:58 -0800

    hidden db variables in .env and now running forever.

--- emergency-response-api/dbcon.js
+++ emergency-response-api/dbcon.js
@@ -1,11 +1,18 @@
+const dotenv = require('dotenv');
+const result = dotenv.config();
+if (result.error) {
+  throw result.error
+}
+console.log(result.parsed);
+
 let mysql = require('mysql');
 
 let pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_peifferp',
-  password        : '2017',
-  database        : 'cs340_peifferp'
+  host            : process.env.DB_HOST,
+  user            : process.env.DB_USER,
+  password        : process.env.DB_PASSWORD,
+  database        : process.env.DB_DATABASE
 });
 
 module.exports.pool = pool
