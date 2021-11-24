295f682eecbdfefbc7083050afbd4256b8de8894
Philip Peiffer <peifferp@flip3.engr.oregonstate.edu>
2021-11-23 18:56:23 -0800

    changed nav-bar so it collapses when screen gets smaller

--- emergency-response-ui/src/components/NavigationBar.js
+++ emergency-response-ui/src/components/NavigationBar.js
@@ -7,15 +7,18 @@ function NavigationBar() {
   return (
     <Navbar variant="dark" expand="lg" bg="dark" fixed="top">
       <Container fluid>
-        <Nav>
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/employees">Employees</Nav.Link>
-          <Nav.Link href="/shifts">Shifts</Nav.Link>
-          <Nav.Link href="/reports">Reports</Nav.Link>
-          <Nav.Link href="/reportemployees">ReportEmployees</Nav.Link>
-          <Nav.Link href="/call-logs">CallLogs</Nav.Link>
-          <Nav.Link href="/employee-shifts">EmployeeShifts</Nav.Link>
-        </Nav>
+      <Navbar.Brand href="/">Home</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className='me-auto'>
+            <Nav.Link href="/employees">Employees</Nav.Link>
+            <Nav.Link href="/shifts">Shifts</Nav.Link>
+            <Nav.Link href="/reports">Reports</Nav.Link>
+            <Nav.Link href="/reportemployees">ReportEmployees</Nav.Link>
+            <Nav.Link href="/call-logs">CallLogs</Nav.Link>
+            <Nav.Link href="/employee-shifts">EmployeeShifts</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
